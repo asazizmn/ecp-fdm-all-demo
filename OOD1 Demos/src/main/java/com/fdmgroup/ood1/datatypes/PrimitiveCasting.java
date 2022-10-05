9dbf7f27ee1e16ced017fb270ca514d57cb2c777
S. Aziz <asazizmn@gmail.com>
2022-10-05 14:36:28 +0800

    add numeric promotion example

--- OOD1 Demos/src/main/java/com/fdmgroup/ood1/datatypes/PrimitiveCasting.java
+++ OOD1 Demos/src/main/java/com/fdmgroup/ood1/datatypes/PrimitiveCasting.java
@@ -11,6 +11,8 @@
 
 package com.fdmgroup.ood1.datatypes;
 
+
+
 public class PrimitiveCasting {
 
 	public static void main(String[] args) {
@@ -34,6 +36,20 @@ public class PrimitiveCasting {
 		// otherwise 'Type mismatch' error​
 		int intType = (int) doubleType; 
 		System.out.println(intType);
+		
+
+		
+		// Numeric promotion (aka implicit casting) ////////////////
+		
+		int intValue = 1;
+		double doubleValue = 1.234;
+		double doubleResult = intValue + doubleValue;
+		System.out.println(doubleResult);
+		
+		long longValue = 1234567879L;
+		float floatValue = 1.23f;
+		float floatResult = longValue + floatValue;
+		System.out.println(floatResult);
 		
 	}
 
