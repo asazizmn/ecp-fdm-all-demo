dfd93f52a6c721a77add272e1d4fb33173876c3e
S. Aziz <asazizmn@gmail.com>
2022-10-12 18:25:25 +0800

    add methods demo

---
+++ OOD1 Demos/src/main/java/com/fdmgroup/ood1/methods/Calculator.java
@@ -0,0 +1,48 @@
+/**
+ * Calculator.java
+ * - methods demonstration
+ *
+ * @author	S. Aziz
+ * @version	0.0.1
+ * @since	12/10/22
+ */
+
+package com.fdmgroup.ood1.methods;
+
+
+
+public class Calculator {
+
+	public static void main(String args[]) {
+		int result = addTwo(2, 2);
+		System.out.println(result);
+	}
+	
+	
+	public static int addTwo(int first, int second) {
+		return first + second;
+	}
+	
+	
+	public static int addThree(int first, int second, int third) {
+		return first + second + third;
+	}
+	
+	
+	public static int addFour(int first, int second, int third, int fourth) {
+		return first + second + third + fourth;
+	}
+	
+	
+	// the varargs ('...') allow 'values' to capture any number of parameters
+	// i.e. add(1, 2, 3, 4, 5) will now work!
+	public static int add(int... values) {
+		int result = 0;
+		for (int value : values) result += value;
+		return result;
+	}
+
+	
+	
+
+}
