20ccb644d162b372d8b0b5ea95f8f9f2bd041612
S. Aziz <asazizmn@gmail.com>
2022-10-12 10:44:34 +0800

    add conditionals demo

---
+++ OOD1 Demos/src/main/java/com/fdmgroup/conditionals/Conditionals.java
@@ -0,0 +1,76 @@
+/**
+ * ArrayBasics.java
+ * - demonstrating basic array usage
+ * - an array is a structure that holds a fixed number of values of the same type
+ *
+ * @author	S. Aziz
+ * @version	0.0.1
+ * @since	11/10/22
+ */
+
+package com.fdmgroup.conditionals;
+
+public class Conditionals {
+
+	public static void main(String[] args) {
+
+		int age = 5;
+		String day = "Sunday";
+
+		
+		// if statement ////////////////////////
+
+		if (age < 16) {
+			System.out.println("child");
+		}
+		
+
+		// if / else if statements ////////////////////////
+
+		if (day.equals("Monday")) {
+			System.out.println("workweek");
+		} 
+		else if (day.equals("Tuesday")) {
+			System.out.println("workweek");
+		} 
+		else if (day.equals("Wednesday")) {
+			System.out.println("workweek");
+		} 
+		else if (day.equals("Thursday")) {
+			System.out.println("workweek");
+		} 
+		else if (day.equals("Friday")) {
+			System.out.println("workweek");
+		} 
+		else if (day.equals("Saturday")) {
+			System.out.println("weekend");
+		} 
+		else if (day.equals("Sunday")) {
+			System.out.println("weekend");
+		} 
+		else {
+			System.out.println("error");
+		}
+
+		
+		// switch statement ////////////////////////
+
+		switch (day) {
+			case "Monday":
+			case "Tuesday":
+			case "Wednesday":
+			case "Thursday":
+			case "Friday":
+				System.out.println("workweek");
+				break;
+			case "Saturday":
+			case "Sunday":
+				System.out.println("weekend");
+				break;
+			default:
+				System.out.println("error");
+				break;
+		}
+
+	}
+}
