24ea35cb5c4415c04e15ef9cc7e005a914b072d5
S. Aziz <asazizmn@gmail.com>
2022-10-25 00:02:33 +0800

    format mockito test demo

--- OOD2 Demos/src/test/java/com/fdmgroup/ood2/mockito/PortfolioTest.java
+++ OOD2 Demos/src/test/java/com/fdmgroup/ood2/mockito/PortfolioTest.java
@@ -17,48 +17,49 @@ import static org.mockito.Mockito.*;
 
 
 public class PortfolioTest {
+
+	Portfolio portfolio;
+	StockService stockService;
+
 	
-   Portfolio portfolio;	
-   StockService stockService;
-	   
-   
-   public static void main(String[] args){
-      PortfolioTest test = new PortfolioTest();
-      test.setUp();
-      System.out.println(test.testMarketValue()?"pass":"fail");
-   }
-   
-   
-   public void setUp(){
-      // create a portfolio object which is to be tested		
-      portfolio = new Portfolio();		
-  
-      // create the mock object of stock service
-      stockService = mock(StockService.class);		
-  
-      // set the stockService to the portfolio
-      portfolio.setStockService(stockService);
-   }
-   
-   
-   public boolean testMarketValue(){
-    	   
-      // creates a list of stocks to be added to the portfolio
-      List<Stock> stocks = new ArrayList<Stock>();
-      Stock googleStock = new Stock("1","Google", 10);
-      Stock microsoftStock = new Stock("2","Microsoft",100);	
- 
-      stocks.add(googleStock);
-      stocks.add(microsoftStock);
+	public void setUp() {
+		// create a portfolio object which is to be tested
+		portfolio = new Portfolio();
 
-      // add stocks to the portfolio
-      portfolio.setStocks(stocks);
+		// create the mock object of stock service
+		stockService = mock(StockService.class);
 
-      // mock the behaviour of stock service to return the value of various stocks
-      when(stockService.getPrice(googleStock)).thenReturn(50.00);
-      when(stockService.getPrice(microsoftStock)).thenReturn(1000.00);		
+		// set the stockService to the portfolio
+		portfolio.setStockService(stockService);
+	}
 
-      double marketValue = portfolio.getMarketValue();		
-      return marketValue == 100500.0;
-   }
+	
+	public boolean testMarketValue() {
+
+		// creates a list of stocks to be added to the portfolio
+		List<Stock> stocks = new ArrayList<Stock>();
+		Stock googleStock = new Stock("1", "Google", 10);
+		Stock microsoftStock = new Stock("2", "Microsoft", 100);
+
+		stocks.add(googleStock);
+		stocks.add(microsoftStock);
+
+		// add stocks to the portfolio
+		portfolio.setStocks(stocks);
+
+		// mock the behaviour of stock service to return the value of various stocks
+		when(stockService.getPrice(googleStock)).thenReturn(50.00);
+		when(stockService.getPrice(microsoftStock)).thenReturn(1000.00);
+
+		double marketValue = portfolio.getMarketValue();
+		return marketValue == 100500.0;
+	}
+	
+
+	public static void main(String[] args) {
+		PortfolioTest test = new PortfolioTest();
+		test.setUp();
+		
+		System.out.println(test.testMarketValue() ? "pass" : "fail");
+	}
 }
