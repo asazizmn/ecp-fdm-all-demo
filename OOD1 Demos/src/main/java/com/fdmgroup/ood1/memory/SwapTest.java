70b86df981489c0af5a8e6f0fdc67ce9ca5c312a
S. Aziz <asazizmn@gmail.com>
2022-10-24 11:47:58 +0800

    improve pass-by-value demos

--- OOD1 Demos/src/main/java/com/fdmgroup/ood1/memory/SwapTest.java
+++ OOD1 Demos/src/main/java/com/fdmgroup/ood1/memory/SwapTest.java
@@ -13,8 +13,8 @@ package com.fdmgroup.ood1.memory;
 
 public class SwapTest {
 	
-	int arg1 = 1;
-	int arg2 = 2;
+	int attr1 = 1;
+	int attr2 = 2;
 	
 	
 	/**
@@ -40,9 +40,9 @@ public class SwapTest {
 	 * @param objRefCopy
 	 */
 	public static void swap( SwapTest objRefCopy ) {
-		int temp = objRefCopy.arg1;
-		objRefCopy.arg1 = objRefCopy.arg2;
-		objRefCopy.arg2 = temp;
+		int temp = objRefCopy.attr1;
+		objRefCopy.attr1 = objRefCopy.attr2;
+		objRefCopy.attr2 = temp;
 	}
 
 	
@@ -65,10 +65,10 @@ public class SwapTest {
 		System.out.println("\nPass values in object");
 		
 		SwapTest swapTest1 = new SwapTest();
-		swap( swapTest1.arg1, swapTest1.arg2 );
+		swap( swapTest1.attr1, swapTest1.attr2 );
 		
-		System.out.println("arg1 is: " + swapTest1.arg1);
-		System.out.println("arg2 is: " + swapTest1.arg2);
+		System.out.println("attr1 is: " + swapTest1.attr1);
+		System.out.println("attr2 is: " + swapTest1.attr2);
 		
 		
 		
@@ -78,7 +78,7 @@ public class SwapTest {
 		SwapTest swapTest2 = new SwapTest();
 		swap( swapTest2 );
 		
-		System.out.println("arg1 is: " + swapTest2.arg1);
-		System.out.println("arg2 is: " + swapTest2.arg2);
+		System.out.println("attr1 is: " + swapTest2.attr1);
+		System.out.println("attr2 is: " + swapTest2.attr2);
 	}
 }
