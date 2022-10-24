4608bfc73a156aa2ddc545ce161ad2ced5aa5256
S. Aziz <asazizmn@gmail.com>
2022-10-24 21:39:43 +0800

    remove unnecessary import from junit demo

--- OOD2 Demos/src/test/java/com/fdmgroup/ood2/junit5/CalculatorTest.java
+++ OOD2 Demos/src/test/java/com/fdmgroup/ood2/junit5/CalculatorTest.java
@@ -15,7 +15,6 @@ import static org.junit.jupiter.api.Assertions.fail;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 
 
