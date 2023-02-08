542c177ac4766ade6663cc7283e0a807bebb4cb0
S. Aziz <asazizmn@gmail.com>
2023-02-08 15:58:30 +0800

    clear tester project

--- Tester/src/com/fdmgroup/tester/packagename/Tester.java
+++ Tester/src/com/fdmgroup/tester/packagename/Tester.java
@@ -1,17 +1,20 @@
+/**
+ * Tester.java
+ * - used as a scratch pad for general java code testing
+ * 
+ * @author	S. Aziz
+ * @version	0.0.1
+ * @since	26/10/22
+ */
+
 package com.fdmgroup.tester.packagename;
 
 
 
 public class Tester {
-
 	public static void main(String[] args) {
-		AnotherRelatedClass xyz = new AnotherRelatedClass();
-		
-		System.out.println(xyz.number);
+
 		
 		
 	}
-	
-	
-
 }
